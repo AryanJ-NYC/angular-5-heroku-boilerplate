90a4ab2f4c45ce71a6fc8afcf43c486c46b47c17
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-09-07 17:14:24 +0000

    Reduce number of files requested upon app startup

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 "use strict";
 const express = require('express'),
       app = express(),
+      morgan = require('morgan'),
+      path = require('path'),
       portNumber = process.env.PORT || process.argv[2] || 8080;
 
-app.use(express.static("./"));  
+app.use(express.static(__dirname)); 
+app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
-  res.render("index.html");
+  res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
 app.listen(portNumber, function () {
--- systemjs.config.js
+++ systemjs.config.js
@@ -1,47 +1,25 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
-(function(global) {
-  // map tells the System loader where to look for things
-  var map = {
-    'app':                        'app', // 'dist',
-    '@angular':                   'node_modules/@angular',
-    'rxjs':                       'node_modules/rxjs'
-  };
-  // packages tells the System loader how to load when no filename and/or no extension
-  var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-  };
-  var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-    'router',
-    'router-deprecated',
-    'upgrade',
-  ];
-  // Individual files (~300 requests):
-  function packIndex(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  }
-  // Bundled (~40 requests):
-  function packUmd(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  }
-  // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  // Add package entries for angular packages
-  ngPackageNames.forEach(setPackageConfig);
-  var config = {
-    map: map,
-    packages: packages
-  };
-  System.config(config);
+(function (global) {
+  System.config({
+    map: {
+      '@angular': 'node_modules/@angular',
+      'rxjs': 'node_modules/rxjs'
+    },
+    paths: {
+      'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
+    },
+    meta: {
+      '@angular/*': {'format': 'cjs'}
+    },
+    packages: {
+      'src': {main: 'main', defaultExtension: 'js'},
+      '@angular/core': {main: 'core.umd.min.js'},
+      '@angular/common': {main: 'common.umd.min.js'},
+      '@angular/compiler': {main: 'compiler.umd.min.js'},
+      '@angular/forms': {main: 'forms.umd.min.js'},
+      '@angular/http': {main: 'http.umd.min.js'},
+      '@angular/platform-browser': {main: 'platform-browser.umd.min.js'},
+      '@angular/platform-browser-dynamic': {main:'platform-browser-dynamic.umd.min.js'},
+      'rxjs': {defaultExtension: 'js'}
+    }
+  });
 })(this);
